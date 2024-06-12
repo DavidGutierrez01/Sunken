72085f573e13ce248ac30076775f1de158d15537
David Gutierrez <davidandrew.gutierrez@benilde.edu.ph>
2024-06-12 18:18:07 +0800

    Initial Commit for player movement

--- Assets/Scripts/Experimental/PlayerMovement.cs
+++ Assets/Scripts/Experimental/PlayerMovement.cs
@@ -1,29 +1,72 @@
+using System;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 
 namespace Experimental
 {
-    [RequireComponent(typeof(Rigidbody))]
+    [RequireComponent(typeof(Rigidbody), typeof(PlayerInput))]
     public class PlayerMovement : MonoBehaviour
     {
-        [SerializeField] private float _speed;
+        [Header("Movement Attributes")]
+        [SerializeField] private float _playerSpeed;
+        [SerializeField] private float _playerJumpHeight;
+
+        [Header("Collision Attributes")]
+        [SerializeField] private bool _showGizmo;
+        [SerializeField] Vector3 _groundColliderSize;
+
 
         #region Components  
         public Rigidbody RigidBody { get; private set; }
+        private PlayerInput _playerInput;
+        #endregion
+
+        #region Private Variables
+
+        private Vector3 _playerPosition;
 
         #endregion
 
-        void Awake()
+        private void Awake()
         {
             RigidBody = GetComponent<Rigidbody>();
+            _playerInput = GetComponent<PlayerInput>();
         }
 
-
-        void FixedUpdate()
+        public void PlayerMove(InputAction.CallbackContext context)
         {
-            Vector3 movementInput = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-            RigidBody.MovePosition(transform.position + movementInput * Time.deltaTime * _speed);
+            _playerPosition = context.ReadValue<Vector3>();
         }
 
+        public void PlayerJump(InputAction.CallbackContext context)
+        {
+            // if (!IsGrounded()) return;
+
+            RigidBody.AddForce(transform.up * _playerJumpHeight, ForceMode.Impulse);
+        }
+
+        private void FixedUpdate()
+        {
+            RigidBody.MovePosition(transform.position + _playerPosition * Time.deltaTime * _playerSpeed);
+        }
+
+        private void OnCollisionEnter(Collision other)
+        {
+
+        }
+
+        private bool IsGrounded()
+        {
+            return Physics.OverlapBox(transform.position, );
+        }
+
+
+        private void OnDrawGizmos()
+        {
+            if (_showGizmo)
+                Gizmos.DrawCube(transform.position, _groundColliderSize);
+        }
     }
 }
