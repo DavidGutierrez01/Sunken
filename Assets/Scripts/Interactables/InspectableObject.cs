d5d4d0c4424693ea4cfaeb6f212dd87a54e92ae5
David Gutierrez <davidandrew.gutierrez@benilde.edu.ph>
2024-06-15 02:31:57 +0800

    Initial Logic for Object Rotation added!

--- Assets/Scripts/Interactables/InspectableObject.cs
+++ Assets/Scripts/Interactables/InspectableObject.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using Cinemachine;
+using UnityEngine.Events;
 
 namespace Interactables
 {
@@ -7,15 +8,47 @@ namespace Interactables
     {
         [SerializeField] private CinemachineVirtualCamera _virtualCam;
         [SerializeField] private float _rotateSpeed = 2f;
+        [SerializeField] private UnityEvent _onObjectInteracted;
+
+        private bool _objectIsActive;
+        private Camera _camera;
+
+        private Vector2 deltaRotation;
+
+        private void Start()
+        {
+            _camera = Camera.main;
+            _objectIsActive = false;
+        }
 
         private void Update()
         {
-
+            if (_objectIsActive)
+            {
+                InspectObject();
+            }
         }
 
         public void Interact()
         {
+            _onObjectInteracted.Invoke();
+            _objectIsActive = !_objectIsActive;
+
+            Debug.Log(_objectIsActive);
+        }
+
+        private void InspectObject()
+        {
             Debug.Log($"Inspecting {this.transform.name}");
+
+            deltaRotation.x = Input.GetAxis("Mouse X");
+            deltaRotation.y = Input.GetAxis("Mouse Y");
+
+            if (Input.GetMouseButton(0) || Input.GetMouseButton(1))
+            {
+                transform.rotation = Quaternion.AngleAxis(deltaRotation.x * _rotateSpeed, _camera.transform.up) *
+                Quaternion.AngleAxis(deltaRotation.y * _rotateSpeed, _camera.transform.right) * transform.rotation;
+            }
         }
     }
 }
